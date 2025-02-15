0ab40b0e7e88281851350b932933b99b62b1c38d
JigneshMahadik <149412247+JigneshMahadik@users.noreply.github.com>
2025-02-15 22:26:21 +0530

    env added

--- Config/db.js
+++ Config/db.js
@@ -1,10 +1,13 @@
 const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const db = mysql.createPool({
-    host: "sql12.freesqldatabase.com",
-    user: "sql12762893",
-    password: "SWCzEZxsYm",
-    database: "sql12762893",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     connectionLimit: 10,
 });
 
